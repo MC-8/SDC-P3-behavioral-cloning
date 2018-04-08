a56aabe99440c6cb2fb89ceeb21bf8f82840bc19
Max Curzi <massimiliano.curzi@gmail.com>
2018-04-08 21:06:02 +0100

    Great results after just 2 epochs

--- clone.py
+++ clone.py
@@ -80,6 +80,6 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 
 model.fit_generator(train_generator, steps_per_epoch=len(train_samples)/BATCH_SIZE, validation_data=validation_generator,
-                    validation_steps=len(validation_samples)/BATCH_SIZE, epochs=3)
+                    validation_steps=len(validation_samples)/BATCH_SIZE, epochs=2)
 model.save('model.h5')
 print("Model saved")
